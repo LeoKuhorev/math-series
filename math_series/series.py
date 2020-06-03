f9dc1c5527612f7e5600c21ff1ff3492932a0d55
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-06-03 15:29:17 -0700

    function with memoization added

--- math_series/series.py
+++ math_series/series.py
@@ -1,11 +1,14 @@
+fib_cache = {}
+
+
 def fibonacci(n: int) -> int:
-    """Function for counting n-th number of fibonacci sequence
+    """Function for counting n-th number of fibonacci sequence using memoization
 
     Args:
-        n (INT): Number of fibonacci sequence
+        n (int): Number of fibonacci sequence
 
     Returns:
-        INT: Value for the n-th number of fibonacci sequence
+        int: Value for the n-th number of fibonacci sequence
     """
     if n < 0:
         return 'Number should be positive'
@@ -13,3 +16,26 @@ def fibonacci(n: int) -> int:
         return n
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def fib_memo(n: int) -> int:
+    """Function for counting n-th number of fibonacci sequence using memoization
+
+    Args:
+        n (int): Number of fibonacci sequence
+
+    Returns:
+        int: Value for the n-th number of fibonacci sequence
+    """
+    if n in fib_cache:
+        val = fib_cache[n]
+    elif n < 0:
+        val = 'Number should be positive'
+    elif n < 2:
+        val = n
+    else:
+        val = fib_memo(n - 1) + fib_memo(n - 2)
+
+    fib_cache[n] = val
+
+    return val
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,58 +1,106 @@
 from math_series import __version__
-from math_series.series import fibonacci
+from math_series.series import fib_memo, fibonacci
 
 
-# Pass test
-def test_fibonacci_pass_1():
-    actual = fibonacci(3)
-    expected = 2
-    assert actual == expected
+class TestFibonacci:
+    """Test class for fibonacci function
+    """
+
+    # Pass test
+    def test_fibonacci_pass_1(self):
+        actual = fibonacci(3)
+        expected = 2
+        assert actual == expected
+
+    # Pass test
+    def test_fibonacci_pass_2(self):
+        actual = fibonacci(7)
+        expected = 13
+        assert actual == expected
+
+    # Pass test
+    def test_fibonacci_pass_3(self):
+        actual = fibonacci(22)
+        expected = 17711
+        assert actual == expected
+
+    # Edge case
+    def test_fibonacci_pass_4(self):
+        actual = fibonacci(0)
+        expected = 0
+        assert actual == expected
+
+    # Edge case
+    def test_fibonacci_pass_5(self):
+        actual = fibonacci(-1)
+        expected = 'Number should be positive'
+        assert actual == expected
+
+    # Fail case
+    def test_fibonacci_fail_1(self):
+        actual = fibonacci(3)
+        expected = 3
+        assert actual != expected
+
+    # Fail case
+    def test_fibonacci_fail_2(self):
+        actual = fibonacci(-1)
+        expected = -1
+        assert actual != expected
+
+    # Fail case
+    def test_fibonacci_fail_3(self):
+        actual = fibonacci(22)
+        expected = 22
+        assert actual != expected
 
 
-# Pass test
-def test_fibonacci_pass_2():
-    actual = fibonacci(7)
-    expected = 13
-    assert actual == expected
+class TestFibMemo:
 
+    # Pass test
+    def test_fib_memo_pass_1(self):
+        actual = fib_memo(3)
+        expected = 2
+        assert actual == expected
 
-# Pass test
-def test_fibonacci_pass_3():
-    actual = fibonacci(22)
-    expected = 17711
-    assert actual == expected
+    # Pass test
+    def test_fib_memo_pass_2(self):
+        actual = fib_memo(7)
+        expected = 13
+        assert actual == expected
 
+    # Pass test
+    def test_fib_memo_pass_3(self):
+        actual = fib_memo(22)
+        expected = 17711
+        assert actual == expected
 
-# Edge case
-def test_fibonacci_pass_4():
-    actual = fibonacci(0)
-    expected = 0
-    assert actual == expected
+    # Edge case
+    def test_fib_memo_pass_4(self):
+        actual = fib_memo(0)
+        expected = 0
+        assert actual == expected
 
+    # Edge case
+    def test_fib_memo_pass_5(self):
+        actual = fib_memo(-1)
+        expected = 'Number should be positive'
+        assert actual == expected
 
-# Edge case
-def test_fibonacci_pass_5():
-    actual = fibonacci(-1)
-    expected = 'Number should be positive'
-    assert actual == expected
+    # Fail case
+    def test_fib_memo_fail_1(self):
+        actual = fib_memo(3)
+        expected = 3
+        assert actual != expected
 
+    # Fail case
+    def test_fib_memo_fail_2(self):
+        actual = fib_memo(-1)
+        expected = -1
+        assert actual != expected
 
-# Fail case
-def test_fibonacci_fail_1():
-    actual = fibonacci(3)
-    expected = 3
-    assert actual != expected
-
-
-# Fail case
-def test_fibonacci_fail_2():
-    actual = fibonacci(-1)
-    expected = -1
-    assert actual != expected
-
-
-# Fail case
-def test_fibonacci_fail_3():
-    actual = fibonacci(22)
-    expected = 22
-    assert actual != expected
+    # Fail case
+    def test_fib_memo_fail_3(self):
+        actual = fib_memo(22)
+        expected = 22
+        assert actual != expected
