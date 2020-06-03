226e0f403669c93c3ee3a57a8230b6f79257385f
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-06-03 15:07:32 -0700

    basic function and tests added

--- math_series/series.py
+++ math_series/series.py
@@ -1,2 +1,15 @@
-def fibonacci(n):
-    return n
+def fibonacci(n: int) -> int:
+    """Function for counting n-th number of fibonacci sequence
+
+    Args:
+        n (INT): Number of fibonacci sequence
+
+    Returns:
+        INT: Value for the n-th number of fibonacci sequence
+    """
+    if n < 0:
+        return 'Number should be positive'
+    elif n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,58 @@
-from math_series import __version__, fibonacci
+from math_series import __version__
+from math_series.series import fibonacci
 
 
-def test_fibonacci_pass():
-    pass
+# Pass test
+def test_fibonacci_pass_1():
+    actual = fibonacci(3)
+    expected = 2
+    assert actual == expected
+
+
+# Pass test
+def test_fibonacci_pass_2():
+    actual = fibonacci(7)
+    expected = 13
+    assert actual == expected
+
+
+# Pass test
+def test_fibonacci_pass_3():
+    actual = fibonacci(22)
+    expected = 17711
+    assert actual == expected
+
+
+# Edge case
+def test_fibonacci_pass_4():
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
+
+
+# Edge case
+def test_fibonacci_pass_5():
+    actual = fibonacci(-1)
+    expected = 'Number should be positive'
+    assert actual == expected
+
+
+# Fail case
+def test_fibonacci_fail_1():
+    actual = fibonacci(3)
+    expected = 3
+    assert actual != expected
+
+
+# Fail case
+def test_fibonacci_fail_2():
+    actual = fibonacci(-1)
+    expected = -1
+    assert actual != expected
+
+
+# Fail case
+def test_fibonacci_fail_3():
+    actual = fibonacci(22)
+    expected = 22
+    assert actual != expected
